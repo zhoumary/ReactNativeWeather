f595b827ae96f9358bed3c926fad7cd1e4c88bfc
1070604284@qq.com <32705597+zhoumary@users.noreply.github.com>
2019-08-16 10:24:31 +0800

    Add files via upload

---
+++ coverage/coverage/forecasts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Forecast extends React.Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }    
+}
+
+export default Forecast;
